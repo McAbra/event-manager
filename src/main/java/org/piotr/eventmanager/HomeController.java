87298804dd39a1e175f2b0a1e42cb00b88b34378
Piotr Olesiejuk <piotro@arg.pl>
2020-04-18 12:39:00 +0200

    HelloController added

---
+++ src/main/java/org/piotr/eventmanager/HomeController.java
@@ -0,0 +1,14 @@
+package org.piotr.eventmanager;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/home")
+    public String home() {
+        return "Hello there";
+    }
+}
