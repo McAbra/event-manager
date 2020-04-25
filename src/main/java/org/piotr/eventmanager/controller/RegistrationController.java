04eb87f84030f7f2920d29f5391027cc5a9400b6
Piotr Grabowicz <p.grabowicz92@gmail.com>
2020-04-25 12:16:21 +0200

    Added Registration Controller registration.html
    Modified UserDTO

---
+++ src/main/java/org/piotr/eventmanager/controller/RegistrationController.java
@@ -0,0 +1,18 @@
+package org.piotr.eventmanager.controller;
+
+import org.piotr.eventmanager.dto.UserDTO;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.context.request.WebRequest;
+
+@Controller
+public class RegistrationController {
+
+    @GetMapping("/user/registration")
+    public String showRegistrationForm(WebRequest request, Model model) {
+        UserDTO userDto = new UserDTO();
+        model.addAttribute("user", userDto);
+        return "registration";
+    }
+}
--- src/main/java/org/piotr/eventmanager/dto/UserDTO.java
+++ src/main/java/org/piotr/eventmanager/dto/UserDTO.java
@@ -3,14 +3,30 @@ package org.piotr.eventmanager.dto;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 @Data
 @NoArgsConstructor
 public class UserDTO {
 
     private Long id;
+
+    @NotNull
+    @NotEmpty
     private String login;
+
+    @NotNull
+    @NotEmpty
     private String password;
+    private String matchingPassword;
+
+    @NotNull
+    @NotEmpty
     private String userName;
+
+    @NotNull
+    @NotEmpty
     private String email;
 
 }
